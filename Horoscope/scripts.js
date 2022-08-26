bdf3a9562f61065f875bd1e18b6a551c4daac72e
Alzter Aquino <alztr2908@gmail.com>
2022-08-26 19:39:24 +0800

    added horoscope file

---
+++ Horoscope/scripts.js
@@ -0,0 +1,13 @@
+// INPUT:
+// Full Name
+// Age
+// Birthday
+// Email
+
+// OUTPUT:
+// Horoscope
+// Message for the day
+
+// FEATURES
+// - output your horoscope based on your bday
+// - email some message from your email to their email
